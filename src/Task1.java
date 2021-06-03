b514ce8c82183013345806c50c8cf1043bafc749
mrusanov <nikolarusano92@gmail.com>
2021-06-03 21:39:12 +0300

    TASk 1 start

--- src/Task1.java
+++ src/Task1.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        double [] inputArr = new double[5];
+        for (int i = 0; i < inputArr.length;){
+            System.out.println("Input Double type value: ");
+            try{
+                inputArr[i] = scanner.nextDouble();
+                i++;
+            }catch (Exception e){
+                System.out.println("Incorrect type data ");
+            }
+        }
     }
 }
